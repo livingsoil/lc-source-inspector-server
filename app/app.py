0d5725679af86dabe4aaa8a93f7b4c14a43569c9
tom <tom.dixon@phoenix.edu>
2024-05-27 10:53:01 -0700

    add post handling endpoint

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return "Hello World!"
+
+@app.route('/scrape', methods=['POST'])
+def scrape():
+    url = request.json.get('url')
+    print(url)
+    return 'URL received'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
